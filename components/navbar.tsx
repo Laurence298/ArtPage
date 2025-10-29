1391be7fefc7529165c04b3fa360339a6c0b3e2f
laurence298 <laurencemunoko@gmail.com>
2025-10-29 11:22:21 -0600

    nav bar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -7,9 +7,10 @@ export default function NavBar() {
     <a className="btn btn-ghost text-xl">daisyUI</a>
   </div>
   <div className="flex-none">
-    <button className="btn btn-square btn-ghost">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block h-5 w-5 stroke-current"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path> </svg>
-    </button>
+    <ul className="menu menu-horizontal px-1">
+      <li><a>Contact</a></li>
+      <li><a>Support</a></li>
+    </ul>
   </div>
 </div>
   );
